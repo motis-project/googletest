337308e8f74d7020c9b21544561e759bbfb01abf
Felix Gündling <felix.guendling@gmail.com>
2020-03-26 20:26:06 +0100

    no duplicate remove assign/move/copy for gtest class

--- googletest/include/gtest/internal/gtest-internal.h
+++ googletest/include/gtest/internal/gtest-internal.h
@@ -1408,8 +1408,6 @@ constexpr bool InstantiateTypedTestCase_P_IsDeprecated() { return true; }
     ~GTEST_TEST_CLASS_NAME_(test_suite_name, test_name)() override = default; \
     GTEST_DISALLOW_COPY_AND_ASSIGN_(GTEST_TEST_CLASS_NAME_(test_suite_name,   \
                                                            test_name));       \
-    GTEST_DISALLOW_MOVE_AND_ASSIGN_(GTEST_TEST_CLASS_NAME_(test_suite_name,   \
-                                                           test_name));       \
                                                                               \
    private:                                                                   \
     void TestBody() override;                                                 \
