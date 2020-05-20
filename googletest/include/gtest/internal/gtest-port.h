5db004e9df2cba54c1c4462de7da8257eff7dee4
Pablo Hoch <pablo@melaxis.com>
2020-05-20 18:00:46 +0200

    fix clang-tidy

--- googletest/include/gtest/internal/gtest-port.h
+++ googletest/include/gtest/internal/gtest-port.h
@@ -682,6 +682,7 @@ typedef struct _RTL_CRITICAL_SECTION GTEST_CRITICAL_SECTION;
 // This should be used in the private: declarations for a class.
 #define GTEST_DISALLOW_COPY_AND_ASSIGN_(type) \
   type(type const&) = delete;                 \
+  type(type&&) = delete;                      \
   type& operator=(type const&) = delete
 
 // A macro to disallow move operator=
